440d478e3579cd20e7cf57c8da8efb87241c4d54
Tolga Kara <tolgakara2.0@gmail.com>
2020-08-24 17:22:21 +0200

    Added some Business Logic

--- dist/script.js
+++ dist/script.js
@@ -10,28 +10,91 @@
 /** String zusammensetzung
  * ROOT : https://www.youtube.com/watch?=
  * VIDKEY : gmyCRJkKeKs
+ *
  */
+let shuffledArrOfShows
+let fiveShows
+let correctAnswer
+let gameRoundQuestion
 
 window.onload = () => {
-	console.log("Loaded Page")
-	//console.log(tvShows)
-	let shuffledArrOfShows = shuffleShowArr()
-	let fiveShows = getFiveObjsFromArr(shuffledArrOfShows)
-	let guessedShowName = getGuessedShow(fiveShows)
-	createTheElements()
+	shuffledArrOfShows = shuffleShowArr(tvShows)
+	fiveShows = getFiveObjsFromArr(shuffledArrOfShows)
+	gameRoundQuestions = generateGameQuestions(fiveShows)
+	playGame(gameRoundQuestions)
+
+	//let guessedShowName = getGuessedShow(fiveShows)
+	//createTheElements()
 	console.log(fiveShows)
 }
 
-function shuffleShowArr() {
-	for (let i = tvShows.length - 1; i > 1; i--) {
+function shuffleShowArr(tvShowsArr) {
+	for (let i = tvShowsArr.length - 1; i > 1; i--) {
 		let j = Math.round(Math.random() * i)
-		let tempCard = tvShows[i]
-		tvShows[i] = tvShows[j]
-		tvShows[j] = tempCard
+		let tempCard = tvShowsArr[i]
+		tvShowsArr[i] = tvShowsArr[j]
+		tvShowsArr[j] = tempCard
 	}
-	return tvShows
+	return tvShowsArr
 }
 
 function getFiveObjsFromArr(shuffledArrOfShows) {
 	return shuffledArrOfShows.slice(0, 5)
+}
+
+function generateGameQuestions(fiveShows) {
+	// Shuffle TV Show
+	let shuffledTVShows = shuffleShowArr(tvShows)
+
+	let counter = 0
+	let listOfQuestions = []
+	let questions = []
+	for (let show of fiveShows) {
+		questions = []
+		console.log(show.title)
+		questions.push(show.title)
+		for (let i = 0; i < shuffledTVShows.length; i++) {
+			if (counter >= 3) {
+				counter = 0
+				break
+			}
+
+			if (show.title !== shuffledTVShows[i].title) {
+				counter++
+				questions.push(shuffledTVShows[i].title)
+			}
+		}
+		listOfQuestions.push(questions)
+	}
+	return listOfQuestions
+}
+
+function playGame(gameRoundQuestions) {
+	let roundIndex = parseInt(document.querySelector("#current-round").innerHTML) - 1
+	correctAnswer = gameRoundQuestions[roundIndex][0]
+	let shuffleGameArr = shuffleShowArr(gameRoundQuestions[roundIndex])
+	changeElementOnWebpage(shuffleGameArr)
+}
+
+function changeElementOnWebpage(gameArr) {
+	console.log(gameArr)
+	document.querySelector("#answer-options > div:nth-child(1) > h1").innerHTML = gameArr[0]
+}
+
+function checkAnswer() {
+	let round = parseInt(document.querySelector("#current-round").innerHTML) + 1
+	let maxRound = parseInt(document.querySelector("#max-round").innerHTML)
+	if (round > maxRound) {
+		showEndScreen()
+	}
+	document.querySelector("#current-round").innerHTML = round
+	console.log("checkAnwer()")
+}
+
+function giveHint() {
+	console.log("giveHint()")
+}
+
+function showEndScreen() {
+	return true
 }
